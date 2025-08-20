edde4d4b49c187e2b1540bf57c2d20c18caf3fa5
Aryan Gidde <giddearyan222@gmail.com>
2025-08-20 16:15:17 +0530

    School Api

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ import mysql from "mysql2/promise";
 import 'dotenv/config';
 
 const db = await mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: process.env.SQLPASS,
-  database: "schooldb"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 try {
